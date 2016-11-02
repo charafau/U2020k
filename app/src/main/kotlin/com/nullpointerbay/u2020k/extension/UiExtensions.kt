b8fde77a9d01475eba59beb1a465bad0b82991d8
charafau <gorudonu@gmail.com>
2016-11-02 17:22:15 +0900

    Added Detail Fragment with anko layout

--- app/src/main/kotlin/com/nullpointerbay/u2020k/extension/UiExtensions.kt
+++ app/src/main/kotlin/com/nullpointerbay/u2020k/extension/UiExtensions.kt
@@ -2,9 +2,12 @@ package com.nullpointerbay.u2020k.extension
 
 
 import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.RequestCreator
 
 //TODO: add more useful extensions like click(observable -> Unit)
 fun Int.dpToPx(context: Context) = this * context.resources.displayMetrics.density
--- app/src/main/kotlin/com/nullpointerbay/u2020k/fragment/DetailFragment.kt
+++ app/src/main/kotlin/com/nullpointerbay/u2020k/fragment/DetailFragment.kt
@@ -1,6 +1,5 @@
 package com.nullpointerbay.u2020k.fragment
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,13 +8,14 @@ import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.android.appKodein
 import com.nullpointerbay.u2020k.R
 import com.nullpointerbay.u2020k.base.BaseFragment
-import com.nullpointerbay.u2020k.extension.inflate
+import com.nullpointerbay.u2020k.base.CircleTransform
 import com.nullpointerbay.u2020k.model.Repo
+import com.squareup.picasso.Picasso
 import org.jetbrains.anko.*
 import org.jetbrains.anko.support.v4.UI
 
 
-class DetailFragment: BaseFragment() {
+class DetailFragment : BaseFragment() {
 
     override fun inject() {
         injector.inject(Kodein {
@@ -43,13 +43,38 @@ class DetailFragment: BaseFragment() {
 
         return UI {
             relativeLayout {
-                verticalLayout {
-                   textView {
-                       text = repo.fullName
+                val IMG_ID = View.generateViewId()
+                val NAME_ID = View.generateViewId()
 
-                   }
+                padding = dip(16)
+                val img = imageView {
+                    id = IMG_ID
+                    lparams(width = dip(40), height = dip(40))
+                }
+
+                textView {
+                    text = repo.name
+                    id = NAME_ID
+                }.lparams {
+                    rightOf(IMG_ID)
+                    leftMargin = dip(16)
+                }
+
+                textView {
+                    text = repo.fullName
+                }.lparams {
+                    rightOf(IMG_ID)
+                    leftMargin = dip(16)
+                    below(NAME_ID)
+                }
+
+                if (repo.owner.avatarUrl != null) {
+                    Picasso.with(context).load(repo.owner.avatarUrl).transform(CircleTransform()).into(img)
+                }else{
+                    img.setImageResource(R.drawable.avatar)
                 }
             }
+
         }.view
 
     }
